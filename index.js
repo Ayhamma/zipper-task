34acfb66b31f3028937d270ee9ca2530f75acd97
Ayhamma <144667767+Ayhamma@users.noreply.github.com>
2026-06-26 10:49:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const app = express();
 const upload = multer();
 const login = "ayham";
 
-app.get("/login", function (req, res) {
+app.get("/login", (req, res) => {
   res.type("text/plain");
   res.send(login);
 });
 
-app.post("/zipper", upload.single("file"), function (req, res) {
-  if (!req.file) {
+app.post("/zipper", upload.any(), (req, res) => {
+  let input;
+
+  if (req.files && req.files.length > 0) {
+    input = req.files[0].buffer;
+  } else {
     return res.status(400).send("no file");
   }
 
-  zlib.gzip(req.file.buffer, function (err, result) {
-    if (err) {
-      return res.status(500).send("error");
-    }
+  zlib.gzip(input, (err, result) => {
+    if (err) return res.status(500).send("error");
 
     res.setHeader("Content-Type", "application/gzip");
     res.setHeader("Content-Disposition", "attachment; filename=result.gz");
